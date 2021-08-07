77ed57a4e64f70869faf015b7f631033ff1c1a1b
cacacaio <cacaioassuncao@hotmail.com>
2021-08-06 23:01:23 -0300

    Reworked a bit of visuals, added title, added movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 import './App.css'
 
 import React, { useEffect } from 'react'
@@ -11,44 +12,112 @@ type games = {
   username: string
   completed: boolean
 }
+type movies = {
+  id: number
+  name: string
+  username: string
+  completed: boolean
+}
 
 function App() {
-  const [state, setState] = React.useState<games[]>([])
-  const [buttonList, setButtonList] = React.useState(true)
-  const [buttonCompleted, setButtonCompleted] = React.useState(false)
+  const [state, setState] = React.useState<games[] | movies[]>([])
+  const [buttonListGames, setButtonList] = React.useState(false)
+  const [buttonCompletedGames, setButtonCompleted] = React.useState(false)
+  const [buttonListMovies, setButtonListMovies] = React.useState(false)
+  const [buttonCompletedMovies, setButtonCompletedMovies] = React.useState(false)
   const [isLoading, setIsLoading] = React.useState(true)
+  const [title, setTitle] = React.useState('')
+
   useEffect(() => {
-    const apiGet = async () => {
+    const apiGetGames = async () => {
+      setButtonList(true)
       const res = await axios.get('https://bot-lya.herokuapp.com/api/games')
       setState(res.data)
       setIsLoading(false)
+      setTitle('Jogos na Lista')
     }
-    apiGet()
+    const apiGetMovies = async () => {
+      setButtonListMovies(true)
+      const res = await axios.get('https://bot-lya.herokuapp.com/api/movies')
+      setState(res.data)
+      setIsLoading(false)
+      setTitle('Filmes na Lista')
+    }
+    if (location.pathname.slice(1) === 'filmes') {
+      apiGetMovies()
+    } else if (location.pathname.slice(1) === 'jogos') {
+      apiGetGames()
+    } else {
+      apiGetGames()
+    }
   }, [])
-  document.title = 'Lista Jogos'
-  const handleListClick = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
+  document.title = 'Lista Filmes / Jogos'
+
+  const handleListClickGames = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault()
+    setIsLoading(true)
     setButtonList(true)
     setButtonCompleted(false)
+    setButtonListMovies(false)
+    setButtonCompletedMovies(false)
+    setTitle('Jogos na Lista')
+
     const res = await axios.get('https://bot-lya.herokuapp.com/api/games')
     const notCompleted = res.data.filter((game: games) => {
       return !game.completed
     })
     setState(notCompleted)
+    setIsLoading(false)
   }
-  const handleCompletedClick = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
+
+  const handleCompletedClickGames = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault()
+    setIsLoading(true)
     setButtonList(false)
     setButtonCompleted(true)
-    setIsLoading(true)
+    setButtonListMovies(false)
+    setButtonCompletedMovies(false)
+    setTitle('Jogos Completos')
+
     const res = await axios.get('https://bot-lya.herokuapp.com/api/games')
     console.log(res.data)
     const completed = res.data.filter((game: games) => {
       return game.completed
+    })
+    setState(completed)
+    setIsLoading(false)
+  }
+
+  const handleListClickMovies = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    setIsLoading(true)
+
+    setButtonList(false)
+    setButtonCompleted(false)
+    setButtonListMovies(true)
+    setButtonCompletedMovies(false)
+    setTitle('Filmes na Lista')
+
+    const res = await axios.get('https://bot-lya.herokuapp.com/api/movies')
+    setIsLoading(false)
+    const completed = res.data.filter((movies: movies) => {
+      return !movies.completed
+    })
+    setState(completed)
+  }
+
+  const handleCompletedClickMovies = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    setIsLoading(true)
+    setButtonList(false)
+    setButtonCompleted(false)
+    setButtonListMovies(false)
+    setButtonCompletedMovies(true)
+    setTitle('Filmes Completos')
+
+    const res = await axios.get('https://bot-lya.herokuapp.com/api/movies')
+    const completed = res.data.filter((movies: movies) => {
+      return movies.completed
     })
     setState(completed)
     setIsLoading(false)
@@ -59,30 +128,44 @@ function App() {
       <header className="App-header">
         <div className="container">
           <button
-            className={buttonList ? 'button button-clicked' : 'button'}
+            className={buttonListMovies ? 'button button-clicked' : 'button'}
             onClick={(e) => {
-              handleListClick(e)
+              handleListClickMovies(e)
+            }}
+          >
+            Filmes Na Lista
+          </button>
+          <button
+            className={buttonCompletedMovies ? 'button button-clicked' : 'button'}
+            onClick={(e) => {
+              handleCompletedClickMovies(e)
+            }}
+          >
+            Filmes Completos
+          </button>
+        </div>
+        <div className="container">
+          <button
+            className={buttonListGames ? 'button button-clicked' : 'button'}
+            onClick={(e) => {
+              handleListClickGames(e)
             }}
           >
             Jogos Na Lista
           </button>
           <button
-            className={buttonCompleted ? 'button button-clicked' : 'button'}
+            className={buttonCompletedGames ? 'button button-clicked' : 'button'}
             onClick={(e) => {
-              handleCompletedClick(e)
+              handleCompletedClickGames(e)
             }}
           >
             Jogos Completos
           </button>
         </div>
         {isLoading && (
-          <ReactLoading
-            type={'spinningBubbles'}
-            color={'white'}
-            height={300}
-            width={300}
-          />
+          <ReactLoading type={'spinningBubbles'} color={'white'} height={300} width={300} />
         )}
+        <h1>{title}</h1>
         <ul>
           {state.map((game, i) => {
             return (
